da66dceb84789cdd3295465affe03d7ca0f79488
Patrick <patrickguan@gmail.com>
2015-05-06 21:06:24 -0400

    features...

--- myfirstforest.py
+++ myfirstforest.py
@@ -11,9 +11,10 @@ import csv as csv
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn import tree
 from sklearn import cross_validation
+from sklearn.cross_validation import KFold, StratifiedKFold, ShuffleSplit
 
 
-def prepareData(data):
+def prepareData(data,train=True):
     
     # Data cleanup
     
@@ -22,12 +23,12 @@ def prepareData(data):
     # I need to fill in the missing values of the data and make it complete.
 
     # female = 0, Male = 1
-    data['Gender'] = data['Sex'].map( {'female': 0, 'male': 1} ).astype(int)
+    data['Gender'] = data['Sex'].map( {'female': 1, 'male': 2} ).astype(int)
     
 
-    data['embark_c'] = data.Embarked.map(lambda x: 1 if x=="C" else 0)
-    data['embark_q'] = data.Embarked.map(lambda x: 1 if x=="Q" else 0)
-    data['embark_s'] = data.Embarked.map(lambda x: 1 if x=="S" else 0)
+    #data['embark_c'] = data.Embarked.map(lambda x: 1 if x=="C" else 0)
+    #data['embark_q'] = data.Embarked.map(lambda x: 1 if x=="Q" else 0)
+    #data['embark_s'] = data.Embarked.map(lambda x: 1 if x=="S" else 0)
     # Embarked from 'C', 'Q', 'S'
     # Note this is not ideal: in translating categories to numbers, Port "2" is not 2 times greater than Port "1", etc.
 
@@ -38,13 +39,20 @@ def prepareData(data):
     Ports = list(enumerate(np.unique(data['Embarked'])))    # determine all values of Embarked,
     Ports_dict = { name : i for i, name in Ports }              # set up a dictionary in the form  Ports : index
     data.Embarked = data.Embarked.map( lambda x: Ports_dict[x]).astype(int)     # Convert all Embark strings to int
-
     
+    #remove null ages
+    if train:
+        data = data[data.Age.notnull()]
+    
+  
+
 
     # All the ages with no data -> make the median of all Ages
     median_age = data['Age'].dropna().median()
     if len(data.Age[ data.Age.isnull() ]) > 0:
         data.loc[ (data.Age.isnull()), 'Age'] = -100
+
+    
 
     # All the missing Fares -> assume median of their respective class
     if len(data.Fare[ data.Fare.isnull() ]) > 0:
@@ -53,8 +61,19 @@ def prepareData(data):
             median_fare[f] = data[ data.Pclass == f+1 ]['Fare'].dropna().median()
         for f in range(0,3):                                              # loop 0 to 2
             data.loc[ (data.Fare.isnull()) & (data.Pclass == f+1 ), 'Fare'] = median_fare[f]
+
+
+    #class sex
+    #data['class_sex'] = data['Gender'] * data['Pclass']
+
+    #sex age
+    #data['age_sex'] = data['Gender'] * data['Age']
+
+    #class fare
+    #data['class_fare'] = data['Pclass'] * data['Fare']
+
     # Remove the Name column, Cabin, Ticket, and Sex (since I copied and filled it to Gender)
-    data = data.drop(['Name', 'Sex', 'Ticket', 'Cabin', 'PassengerId','Embarked'], axis=1)
+    data = data.drop(['Name', 'Sex', 'Ticket', 'Cabin', 'PassengerId','Embarked','SibSp','Parch','Pclass','Age'], axis=1)
 
     return data
 
@@ -70,8 +89,8 @@ if __name__ == '__main__':
     # Collect the test data's PassengerIds before dropping it
     ids = test_df['PassengerId'].values
 
-    train_df = prepareData(train_df)
-    test_df = prepareData(test_df)
+    train_df = prepareData(train_df, True)
+    test_df = prepareData(test_df, False)
 
     print train_df.columns
 
@@ -81,28 +100,45 @@ if __name__ == '__main__':
     test_data = test_df.values
 
     print 'Training...'
-    forest = AdaBoostClassifier(n_estimators=250)
-    #forest = RandomForestClassifier(n_estimators=250)
-    #forest = tree.DecisionTreeClassifier()
+    #forest = AdaBoostClassifier(n_estimators=100)
+    #forest = RandomForestClassifier(n_estimators=25)
+    forest = tree.DecisionTreeClassifier()
 
-    scores = cross_validation.cross_val_score(forest, train_data[0::,1::], train_data[0::,0], cv=5)
-    print 'cross validate scores',scores
+    #scores = cross_validation.cross_val_score(forest, train_data[0::,1::], train_data[0::,0], cv=5)
+    #print 'cross validate scores',scores
 
 
-    forest = forest.fit( train_data[0::,1::], train_data[0::,0] )
+    #forest = forest.fit( train_data[0::,1::], train_data[0::,0] )
+    #print train_data[0::,0]
+    clfs=[]
+    #folds = StratifiedKFold(train_data[0::,0], 10)
+    #folds = KFold(len(train_data), 10)
+    folds = ShuffleSplit(len(train_data), n_iter=20, test_size=0.10,random_state=0)
+    scores=[]
+    for i,(train,test) in enumerate(folds):
+        clfs+=[forest.fit(train_data[0::,1::][train], train_data[0::,0][train])]
+        scores+=[forest.score(test_data,answers_targets)]
     
-    print 'Predicting...'
-    output = forest.predict(test_data).astype(int)
-    print len(test_data)
-    print len(answers_targets)
-    score = forest.score(test_data,answers_targets)
-    print "score",score
+    #scores=[]        
+   # for clf in clfs:
+   #     output = clf.predict(test_data).astype(int)
+    #    score = forest.score(test_data,answers_targets)
+     #   scores+=[score]
+    print scores
 
 
-    predictions_file = open("myfirstforest.csv", "wb")
-    open_file_object = csv.writer(predictions_file)
-    open_file_object.writerow(["PassengerId","Survived"])
-    open_file_object.writerows(zip(ids, output))
-    predictions_file.close()
+   # print 'Predicting...'
+   # output = forest.predict(test_data).astype(int)
+   #print len(test_data)
+    #print len(answers_targets)
+    #score = forest.score(test_data,answers_targets)
+    #print "score",score
+
+
+   # predictions_file = open("myfirstforest.csv", "wb")
+   # open_file_object = csv.writer(predictions_file)
+   # open_file_object.writerow(["PassengerId","Survived"])
+   # open_file_object.writerows(zip(ids, output))
+   # predictions_file.close()
     print 'Done.'
     
